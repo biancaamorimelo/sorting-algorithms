edc8a88cb52562a342111abad526e9ffa2c19098
Bianca Amorim <biancaamoriim@gmail.com>
2019-02-07 11:39:01 -0200

    Inclusão de algoritmo
    Inclusão de algoritmo SelectionSort da atividade anterior.

---
+++ selectionSort.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+    /***************************************************************************
+     *  Aluna:          Bianca Karla Amorim de S. Melo 							*
+     *	Matricula:      11408143                         						*
+     *  									 							        *	 
+     *  Implementacao do algoritmo SelectionSort.    							*         
+     *  									 							        *
+     **************************************************************************/
+
+void selectionSort(int *V, int n){
+	int i, j;
+	int i_min, temp;
+	
+	for (i = 0; i < n-1; i++){
+		i_min = i;
+		for(j = i+1; j < n; j++){ 
+			if(V[j] < V[i_min]){ //Procura o menor elemento
+				i_min = j;  //Atualiza o valor da menor posição
+			}
+		}
+		if(i != i_min){  //Troca o valor das posições atual e menor
+			temp = V[i];
+			V[i] = V[i_min];
+			V[i_min] = temp; 
+		}
+	}
+}    
+
+
+int main(){
+	
+	int i, numElementos;
+	int *V;
+	
+	printf("Informe o tamanho do vetor a ser ordenado: \n");
+	scanf("%d", &numElementos);	
+	
+	V = (int*)malloc(numElementos*sizeof(int));
+	
+	printf("Informe o vetor a ser ordenado: \n");
+	for(i = 0; i < numElementos; i++){
+		scanf("%d", &V[i]);
+	}
+	
+	//Exibe os elementos do array antes de serem ordenados.
+	printf("Array desordenado: ");
+	for(i = 0; i < numElementos; i++){
+		printf("%d ", V[i]);
+	}
+	
+	printf("\n");
+	
+	//Chamada da funcao para ordenacao dos elementos.
+	selectionSort(V, numElementos);
+	
+	//Exibe os elementos do array ordenados.
+	printf("Array ordenado: ");
+	for(i = 0; i < numElementos; i++){
+		printf("%d ", V[i]);
+	}
+	
+	return 0;
+     	
+}
